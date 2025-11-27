ca0ee8f2c1e64cc6229c9600bc05f5bf3859ea17
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:11:10 -0300

    aaa

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "meu-bucket-localstack"
+  bucket = var.bucket_name
 }
--- provider.tf
+++ provider.tf
@@ -2,21 +2,23 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  s3_use_path_style           = true
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
   endpoints {
-    s3 = "http://localhost:4566"
+    s3 = "http://localstack:4566"
+    iam = "http://localstack:4566"
+    sts = "http://localstack:4566"
   }
 }
