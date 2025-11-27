0a5878f87ddc28aeffe8b26e5035c0c9b4a72396
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:58:24 -0300

    checked 11

--- aws.provider.tf
+++ aws.provider.tf
@@ -1,14 +1,17 @@
 provider "aws" {
-  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
+  region                      = "us-east-1"
 
-  skip_requesting_account_id  = true
+  # Permite endpoints locais
   skip_credentials_validation = true
+  skip_metadata_api_check     = true
   skip_region_validation      = true
 
-  endpoints {
-  s3 = "http://localstack:4566"
-}
+  s3_use_path_style = true
 
+  endpoints {
+    s3         = "http://localstack:4566"
+    s3control  = "http://localhost" # bloqueia chamada ao S3 Control
+  }
 }
--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "bucket-localstack-teste"
-
+  bucket        = "bucket-localstack-teste"
   force_destroy = true
-
-  lifecycle {
-    ignore_changes = [
-      tags_all
-    ]
-  }
 }
