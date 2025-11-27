6f75172035069c1ff767a963ff2e9d6811ccbfce
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 16:07:56 -0300

    checked13

--- aws.provider.tf
+++ aws.provider.tf
@@ -1,17 +1,12 @@
 provider "aws" {
+  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  region                      = "us-east-1"
-
-  # Permite endpoints locais
   skip_credentials_validation = true
+  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
-  skip_region_validation      = true
-
-  s3_use_path_style = true
 
   endpoints {
-    s3         = "http://localstack:4566"
-    s3control  = "http://localhost" # bloqueia chamada ao S3 Control
+    s3 = "http://localhost:4566"
   }
 }
