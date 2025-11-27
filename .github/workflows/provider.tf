e49be202c8eba73a92bc1d32b2ed049058d7328c
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 14:29:48 -0300

    ttt

---
+++ .github/workflows/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
