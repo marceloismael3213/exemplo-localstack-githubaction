268290d83773b34e44bd8bcd03eb986919627d53
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:29:42 -0300

    checked3

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "bucket-localstack-teste"
 }
