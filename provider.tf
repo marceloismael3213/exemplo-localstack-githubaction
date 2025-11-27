77c91961b0a22209747296735471c8be75bc4dc7
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:32:07 -0300

    checke4

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
 
-  s3_use_path_style = true
-
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+  skip_region_validation      = true
 
   endpoints {
     s3 = "http://localhost:4566"
