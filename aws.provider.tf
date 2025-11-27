4412159bc9076906a449272f083cb9156ce33609
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:43:20 -0300

    checked8

--- aws.provider.tf
+++ aws.provider.tf
@@ -4,9 +4,15 @@ provider "aws" {
   region                      = "us-east-1"
 
   s3_use_path_style           = true
+
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_region_validation      = true
+  skip_metadata_api_check     = true
+  skip_s3_checksum            = true
+
+  # ESSENCIAL PARA EVITAR O ERRO
+  skip_s3_control_endpoint    = true
 
   endpoints {
     s3 = "http://localhost:4566"
