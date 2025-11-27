bfcffeb2e7dfdfe4d8d6dbc4365cad91a4723dba
marceloismael3213 <50075778+marceloismael3213@users.noreply.github.com>
2025-11-27 15:18:26 -0300

    checked

--- provider.tf
+++ provider.tf
@@ -9,16 +9,17 @@ terraform {
 }
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
+  region     = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+
+  s3_use_path_style = true
+
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  s3_use_path_style           = true
+  skip_metadata_api_check     = true
 
   endpoints {
     s3 = "http://localstack:4566"
-    iam = "http://localstack:4566"
-    sts = "http://localstack:4566"
   }
 }
